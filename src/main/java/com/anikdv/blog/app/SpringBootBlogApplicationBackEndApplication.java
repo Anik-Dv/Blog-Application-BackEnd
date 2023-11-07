3f33ff2dc8a817098bacfa30d421c030e34fac4b
Anik-Dv <freelanceranik50@gmail.com>
2023-11-08 02:08:09 +0600

    project initialization

---
+++ src/main/java/com/anikdv/blog/app/SpringBootBlogApplicationBackEndApplication.java
@@ -0,0 +1,19 @@
+package com.anikdv.blog.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ */
+@SpringBootApplication
+public class SpringBootBlogApplicationBackEndApplication {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBlogApplicationBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anikdv/blog/app/SpringBootBlogApplicationBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anikdv.blog.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBlogApplicationBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
